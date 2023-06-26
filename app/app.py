6be16f67392f272e70643e2b58b96f317b0f4917
Runar Fosse <RUNF@equinor.com>
2023-06-26 13:27:11 +0200

    Added endpoint for getting object as resqml given uuid

--- app/app.py
+++ app/app.py
@@ -14,12 +14,33 @@ if __name__ == "__main__":
 def hello_world():
     return "<p> Hello world! </p>"
 
+
+app.get("/object/")
+def get_object_as_resqml():
+    uuid = request.args.get("id")
+    if not uuid:
+        return "Missing object id", 400
+    
+    token = request.headers.get("Authorization")
+    if not token:
+        return "Missing authorization token in header", 401
+    
+    token = token.split(" ")[1]
+    sumo = Explorer("dev", token)
+
+    object = sumo.cases.filter(uuid=uuid)
+    if not object:
+        return "Object not found", 400
+    
+    return json_to_resqml(object[0])
+
+
 # Retrieve sample case
-@app.get("/sample-resqml")
+@app.get("/sample-resqml/")
 def sample_resqml():
     token = request.headers.get("Authorization")
     if not token:
-        return "Missing authorization token", 401
+        return "Missing authorization token in header", 401
     
     token = token.split(" ")[1]
     
