d3ecfbe364ce3bfca1ac0c5ed9dba0e3356e9db0
Runar Fosse <RUNF@equinor.com>
2023-06-26 12:35:46 +0200

    Added sample endpoint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, send_file
 
+from encoding import json_to_resqml
+
+from fmu.sumo.explorer import Explorer
+sumo = Explorer("dev")
+
+# Run application
 app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+
 
 @app.get("/")
 def hello_world():
     return "<p> Hello world! </p>"
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+# Retrieve sample case
+@app.get("/sample-resqml")
+def sample_resqml():
+    polygon = sumo.cases.filter(asset="Drogon")[1].polygons[0]
+    return f"<textarea style='width:90vw;height:90vh;border:none;'>{json_to_resqml(polygon.metadata)}</textarea>"
