ef8159f678fdba4896dfe16f01bd249792a80531
Runar Fosse <RUNF@equinor.com>
2023-07-03 13:03:57 +0200

    Added resqpy dependency

--- app/utility.py
+++ app/utility.py
@@ -15,7 +15,24 @@ def convert_objects_to_resqml(uuids : list[str], sumo : Explorer) -> tuple(Bytes
 
         Returns two different zip files, first containing EPC files, second HDF files.
     """
-    pass
+
+    # Open two Bytestreams, one for each filetype
+    epcstream, hdfstream = BytesIO(), BytesIO()
+
+    # Open said Bytestreams as ZipFiles
+    with ZipFile(epcstream, "w") as epcf, ZipFile(hdfstream, "w") as hdff:
+        
+        # Iterate over all given uuids
+        for uuid in uuids:
+            # Convert current object to RESQML
+            epc, hdf = convert_object_to_resqml(uuid, sumo)
+
+            # Insert both those files (bytestreams) into each respective zipstream
+            epcf.write(f"{uuid}.epc", epc.getvalue())
+            hdff.write(f"{uuid}.h5", hdf.getvalue())
+
+    # Return both zipstreams
+    return epcstream, hdfstream
 
 
 def convert_object_to_resqml(uuid : str, sumo : Explorer) -> tuple(BytesIO, BytesIO):
