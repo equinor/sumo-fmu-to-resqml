f0262bfb8fa5c4929acc8f740935a92a52188909
Runar Fosse <RUNF@equinor.com>
2023-07-04 13:37:37 +0200

    Removed deprecated features

--- app/_deprecated/deprecated.py
+++
@@ -1,6 +0,0 @@
-# Deprecation warning decorator
-from warnings import warn
-
-def deprecated(func) -> None:
-    warn(f"Function {func.__name__} is deprecated.")
-    return func
--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-from _deprecated._functionality import *
 from functionality import *
 
 
@@ -8,17 +7,11 @@ from functionality import *
 app = Flask(__name__)
 
 
-# Route endpoints to functions (Deprecated)
-app.add_url_rule("/depr/objects/", methods=["GET"], view_func=get_objects)
-app.add_url_rule("/depr/objects/data", methods=["GET"], view_func=get_objects_hdf)
-
-app.add_url_rule("/depr/objects/", methods=["POST"], view_func=get_several_objects)
-app.add_url_rule("/depr/objects/data", methods=["POST"], view_func=get_several_objects_hdf)
-
-# (Not deprecated)
+# Route endpoints to functions
 app.add_url_rule("/objects/", methods=["GET", "POST"], endpoint="resqml-retrieval", view_func=get_resqml)
 app.add_url_rule("/objects/epc", methods=["GET", "POST"], endpoint="epc-retrieval", view_func=get_epc)
 app.add_url_rule("/objects/hdf", methods=["GET", "POST"], endpoint="hdf-retrieval", view_func=get_hdf)
+
 
 @app.get("/")
 def hello_world():
