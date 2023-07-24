dd8aeca46b15fbfc0c2b313ad0a91f86c078f5a6
Runar Fosse <RUNF@equinor.com>
2023-07-24 09:52:30 +0200

    Added 'object type implemented check' wrapper

--- fmu-to-resqml/wrappers.py
+++ fmu-to-resqml/wrappers.py
@@ -1,3 +1,18 @@
 """
     All wrapper functions
 """
+
+def try_object_implemented(func): 
+    def wrapper(*args, **kwargs):
+        try:
+            # Try to return the function value
+            return func(*args, **kwargs)
+        except Exception as e:
+            # If the function returns a "Not implemented" error, return the exception arguments
+            if len(e.args) > 1 and e.args[1] == 501:
+                return e.args
+            
+            # If not, raise the exception further
+            raise e
+    
+    return wrapper
