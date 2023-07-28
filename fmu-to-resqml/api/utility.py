1afda9c549311e3f36ed502676e52a0e4725e388
Runar Fosse <RUNF@equinor.com>
2023-07-28 14:03:23 +0200

    Readded exception throw to debug mesh error

--- fmu-to-resqml/api/utility.py
+++ fmu-to-resqml/api/utility.py
@@ -71,6 +71,7 @@ def convert_ensemble_to_resqml(uuid : str, iterations : list[str], tagnames : li
 
     # Then we write and store the output of the model into temporary files
     # Write to epc file
+    raise Exception(meshes[0].model, meshes[0].model.parts(), model, model.parts())
     for crs in crss.values():
         crs.create_xml()
     for mesh in meshes:
