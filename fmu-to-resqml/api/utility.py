c9ce207bb0b59ef68079cb7bd462389de7741b0e
Runar Fosse <RUNF@equinor.com>
2023-07-28 12:33:18 +0200

    Added debugging of model parts

--- fmu-to-resqml/api/utility.py
+++ fmu-to-resqml/api/utility.py
@@ -67,6 +67,7 @@ def convert_ensemble_to_resqml(uuid : str, iterations : list[str], tagnames : li
 
     # Then we write and store the output of the model into temporary files
     # Write to epc file
+    raise Exception(model.parts())
     for crs in crss.values():
         crs.create_xml()
     for mesh in meshes:
