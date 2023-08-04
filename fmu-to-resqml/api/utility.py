ff80c227e230af337089c850209005282e6574cd
Runar Fosse <RUNF@equinor.com>
2023-08-04 09:03:49 +0200

    Debug SUMO case filtering

--- fmu-to-resqml/api/utility.py
+++ fmu-to-resqml/api/utility.py
@@ -51,6 +51,7 @@ def convert_ensemble_to_resqml(uuid : str, iterations : list[str], tagnames : li
 
     # First we do surfaces (meshes)
     surfaces = case.surfaces.filter(iteration=iterations, tagname=tagnames, name=names)
+    raise Exception(surfaces)
     for surface in surfaces:
         # Ensure that object is a realization
         if surface.realization == None:
